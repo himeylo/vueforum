576ab6617e0ac52fec4cea0003a947c2e77c979e
Jamie McKinnerney <18506997+himeylo@users.noreply.github.com>
2022-04-24 11:50:08 -0500

    Progress

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PageHome from '@/components/PageHome'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+	{ 
+		path: '/', 
+		name: 'Home',
+		component: PageHome 
+	}
+ ]
+
+ const router = createRouter({
+	history: createWebHistory(),
+	routes
+ })
+
+const forumApp = createApp(App)
+forumApp.use(router)
+forumApp.mount('#app')
