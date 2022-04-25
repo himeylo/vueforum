c3edbcb8ada10c2b4006c95fa716ec2a28f61ca0
Jamie McKinnerney <18506997+himeylo@users.noreply.github.com>
2022-04-25 01:21:05 -0500

    Set -prefixed components to be set as global components automatically; Displays localized, readable date in tooltips across the forum

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -10,7 +10,7 @@
               <router-link :to="{name: 'ThreadShow', params: {id: thread.id}}">{{ thread.title }}</router-link>
           </p>
           <p class="text-faded text-xsmall">
-              By <a href="#">{{ userById(thread.userId).name }}</a>, {{ thread.publishedAt }}.
+              By <a href="#">{{ userById(thread.userId).name }}</a>, <AppDate :timestamp="thread.publishedAt" />.
           </p>
         </div>
 
@@ -25,7 +25,7 @@
                 <p class="text-xsmall">
                     <a href="#">{{ userById(thread.userId).name }}</a>
                 </p>
-                <p class="text-xsmall text-faded">{{ thread.publishedAt }}</p>
+                <p class="text-xsmall text-faded"><AppDate :timestamp="thread.publishedAt" /></p>
             </div>
         </div>
       </div>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,18 @@ import router from '@/router'
 
 const forumApp = createApp(App)
 forumApp.use(router)
+
+// This sets our "App" prefixed components as global automatically
+const requireComponent = require.context('./components', true, /App[A-Z]\w+\.(vue|js)$/) // App is our global base indicator
+requireComponent.keys().forEach(function (fileName) {
+  let baseComponentConfig = requireComponent(fileName)
+  baseComponentConfig = baseComponentConfig.default || baseComponentConfig
+  const baseComponentName = baseComponentConfig.name || (
+    fileName
+      .replace(/^.+\//, '')
+      .replace(/\.\w+$/, '')
+  )
+  forumApp.component(baseComponentName, baseComponentConfig)
+})
+
 forumApp.mount('#app')
