9657956da49065c397a0649f3417e9a7b936fe43
Jamie McKinnerney <18506997+himeylo@users.noreply.github.com>
2022-04-25 00:56:08 -0500

    Use dayJS to format dates for relative reference and using localization

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -29,8 +29,8 @@
           </div>
         </div>
 
-        <div class="post-date text-faded">
-          {{ post.publishedAt }}
+        <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)">
+          {{ diffForHumans(post.publishedAt) }}
         </div>
       </div>
     </div>
@@ -38,6 +38,12 @@
 
 <script>
 import sourceData from '@/data.json'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedDate from 'dayjs/plugin/localizedFormat'
+dayjs.extend(relativeTime)
+dayjs.extend(localizedDate)
+
 export default {
   props: {
     posts: {
@@ -53,6 +59,12 @@ export default {
   methods: {
     userById (userId) {
       return this.users.find((p) => p.id === userId)
+    },
+    diffForHumans (timestamp) {
+      return dayjs.unix(timestamp).fromNow()
+    },
+    humanFriendlyDate (timestamp) {
+      return dayjs.unix(timestamp).format('llll')
     }
   }
 }
