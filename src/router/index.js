112a12e2e676c87a9454b26fc9a0db81797f1510
Jamie McKinnerney <18506997+himeylo@users.noreply.github.com>
2022-04-24 21:20:36 -0500

    pre 4.2: Creating a forum post component

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,34 @@
-import Home from '@/components/PageHome'
-import ThreadShow from '@/components/PageThreadShow'
+import PageHome from '@/components/PageHome'
+import PageThreadShow from '@/components/PageThreadShow'
 import NotFound from '@/components/PageNotFound'
 import { createRouter, createWebHistory } from 'vue-router'
+import sourceData from '@/data.json'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: PageHome
   },
   {
     path: '/thread/:id',
     name: 'ThreadShow',
-    component: ThreadShow,
-    props: true
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFound',
-    component: NotFound
+    component: PageThreadShow,
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exists
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+      // if exists continue
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          component: NotFound,
+          path: { pathMatch: to.path.substring(1).split('/') }
+        })
+      }
+    }
   }
 ]
 
